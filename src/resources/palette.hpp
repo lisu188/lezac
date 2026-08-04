458de57677059b5362ad4d2ff8782366e6ac51f8
lisu188 <andrzej.lis3@gmail.com>
2026-08-04 07:42:25 +0200

    Expose shared VGA palette conversion

--- src/resources/palette.hpp
+++ src/resources/palette.hpp
@@ -9,6 +9,7 @@
 
 namespace lezac::resources {
 
+uint8_t vga6To8(uint8_t v) noexcept;
 Palette loadPalette(const std::vector<uint8_t>& data, std::size_t off);
 Palette loadPaletteFile(const std::string& path);
 
